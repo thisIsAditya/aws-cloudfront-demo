109ecf6264ebb19d89fdd97ee11c3f3f7ea27620
Aditya Pandey <pandey.api2k@gmail.com>
2023-04-14 01:33:23 +0530

    Integrated the cloundfront SDk

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ const SECRETS = {
   NODE_ENV: process.env.NODE_ENV,
   CLOUDFRONT_URI: process.env.CLOUDFRONT_URI,
   DEFAULT_MEDIA: "hello-world-at-cloud.png",
+  CDN_KEY_PAIR_ID: process.env.CDN_KEY_PAIR_ID,
+  CDN_PRIVATE_KEY: process.env.CDN_PRIVATE_KEY,
 };
 
 export default SECRETS;
--- src/controllers/cdn-controller/index.js
+++ src/controllers/cdn-controller/index.js
@@ -1,15 +1,22 @@
 import handleError from "../../utils/handleError.js";
 import APIS from "../../constants/apis.js";
 import SECRETS from "../../config.js";
+import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 
 export const getMediaLink = handleError(async (req, res) => {
   let { mediaName } = req.params;
   if (!mediaName) mediaName = SECRETS.DEFAULT_MEDIA;
   const imageURL = `${APIS.MEDIA.get}/${mediaName}`;
+  const signedURL = getSignedUrl({
+    url: imageURL,
+    dateLessThan: new Date(Date.now() + 5 * 60 * 1000) /** 5 mins from now */,
+    privateKey: SECRETS.CDN_PRIVATE_KEY,
+    keyPairId: SECRETS.CDN_KEY_PAIR_ID,
+  });
   res.status(200).send({
     status: "success",
     data: {
-      imageURL,
+      imageUrl: signedURL,
     },
   });
 });
